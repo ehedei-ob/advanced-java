057034fed9af6d4af70efde2ee2c1e6b75ee4f5c
Ehedei <ehedeidev@gmail.com>
2023-07-11 16:47:07 +0200

    Added comments

--- src/main/java/org/example/singleton/WebClient.java
+++ src/main/java/org/example/singleton/WebClient.java
@@ -2,12 +2,19 @@ package org.example.singleton;
 
 import java.util.Objects;
 
+/**
+ * Singleton web client to fetch information from API
+ */
 public class WebClient {
     private static WebClient webClient;
     private String url = "https://bing.com";
 
     private WebClient() {}
 
+    /**
+     * Singleton entry point
+     * @return Web client
+     */
     public static WebClient getInstance() {
         if (Objects.isNull(webClient)) {
             webClient = new WebClient();
