4f757b6cb610c031bde9dfed42811b563ccff64d
Ehedei <ehedeidev@gmail.com>
2023-07-12 11:14:11 +0200

    Ex 15-16-17-18

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,29 @@
 package org.example;
 
-import org.example.singleton.WebClient;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) {
-        final WebClient webClient1 = WebClient.getInstance();
-        final WebClient webClient2 = WebClient.getInstance();
-
-        if (webClient1 == webClient2) {
-            System.out.println("Web client is the same instance");
-        } else {
-            System.out.println("Singleton pattern bad implemented");
+    public static void main(final String[] args) {
+        try {
+            int num = 8/0;
+        } catch (final ArithmeticException e) {
+            System.out.println(e.getMessage());
         }
 
-        webClient1.setUrl("https://google.es");
+        final int[] array = { 1, 2, 3, 4, 5, 6 };
 
-        if (webClient1.getUrl().equals(webClient2.getUrl())) {
-            System.out.println("Web client is the same instance and their url are the same");
-        } else {
-            System.out.println("Singleton pattern bad implemented");
+        try {
+            int num = array[8];
+        } catch (final ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            final FileReader fileReader = new FileReader("myfile.txt");
+        } catch (final FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
 
     }
